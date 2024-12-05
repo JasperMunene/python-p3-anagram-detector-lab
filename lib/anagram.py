4d43b72240f1c422c7a5efc5aaeb47fb8fdd0656
JasperMunene <jaspermunene2022@gmail.com>
2024-12-05 12:55:57 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words_list):
+        result = []
+        for word in words_list:
+            if sorted(self.word) == sorted(word):
+                result.append(word)
+        return result
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
